17c38dfcaaa9be68aeaba879186fb692293ea988
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2021-07-15 18:55:16 -0500

    part 4

---
+++ pointOfSale/pointOfSale/pointOfSale.Shared/Pages/LoginPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using pointOfSale.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace pointOfSale.Pages
+{
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool IsValid = await ValidForm();
+            if (!IsValid)
+            {
+                return;
+            }
+
+            MessageDialog messageDialog = new MessageDialog("Hell Yeah!", "ok");
+            await messageDialog.ShowAsync();
+        }
+
+        private async Task<bool> ValidForm()
+        {
+            MessageDialog messageDialog;
+
+            if (string.IsNullOrEmpty(EmailTextBox.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu email");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            if (!RegexUtilities.IsValidEmail(EmailTextBox.Text))
+            {
+                messageDialog = new MessageDialog("Debes Ingresar un e-mail válido", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            if (PasswordPasswordBox.Password.Length < 6)
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu contraseña");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
